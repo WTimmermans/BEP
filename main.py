485616081aaecdf1c592325e44f59cd236c4ea2f
Wouter Timmermans <wtimmermans2003@gmail.com>
2025-05-25 12:06:34 +0200

    Beam selection is ook terug

--- main.py
+++ main.py
@@ -32,24 +32,47 @@ scale = 1
 calibration_text = ""
 calibration_counter = 0
 known_distance_mm = 500
-
 beam_list = ["Square aluminium", "C profile aluminium", "Hollow round steel", "Solid round steel", "Solid round POM"]
+# MAYBE AT 'U'SECTION (90 DEG ROTATED C SECTION)
 
-# Beam Properties
+# Beam Properties (Use elif statements to add more profiles)
 def beam_props(beam_select):
+    # Square Aluminium
     if beam_select == 0:
-        E = 69e9 # Young's Modulus Aluminium (Pa)
-        b = 0.01 # Outer length (m)
-        t = 0.001# Thickness (m)
+        E = 69e9   # Young's Modulus Aluminium (Pa)
+        b = 0.01   # Outer length (m)
+        t = 0.001  # Thickness (m)
         I = ((b**4) - (b - 2*t)**4) / 12  # Second moment of interia (m^4)
-        EI = E*I
-        
-    # Use more if statements here to add different beams
-
+    
+    # C Profile Aluminium
     elif beam_select == 1:
-        #dummywaardes
-        EI = 20
+        E = 69e9 # Young's Modulus Aluminium (Pa)
+        t = 1e-3  # thickness (m)
+        h = 10e-3  # height (m)
+        b = 10e-3  # width (m)
+        I = ((1/12)*t*(h-2*t)**3)+2*(((1/12)*b*t**3)+((b*t)*(((h/2)-(t/2))**2)))
         
+    # Hollow Round Steel    
+    elif beam_select == 2:
+        E = 200e9 # Young's Modulus Steel (Pa)
+        R = 6e-3 # External radius (m)
+        r = 5e-3 # Internal radius (m)
+        I = (np.pi/4)*(R**4-r**4) #m^2
+    
+    # Solid Round Steel
+    elif beam_select == 3:
+        E = 200e9 # Young's Modulus Steel (Pa)
+        R = 4e-3 # External radius (m)
+        I = (np.pi/4)*R**4 #m^4
+      
+    # Solid Round POM
+    elif beam_select == 4:
+         E = 2700e6 # Young's Modulus POM (Pa)
+         R = 5e-3 # External radius (m)
+         I = (np.pi/4)*R**4 #m^4
+    
+    EI = E*I
+    
     else:
         print("Error: Please select beam.")
 
